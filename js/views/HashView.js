551b2d06335d49e7cfd650ba7794e9bebd309133
Willem Ellis <willem.ellis@gmail.com>
2014-04-29 20:19:24 -0400

    implement infinite scrolling! #10

--- js/views/HashView.js
+++ js/views/HashView.js
@@ -17,33 +17,50 @@ define([
 		initialize: function(options) {
 			this.options = options;
 		},
+		events: {
+			'click #new-post-alert-container': 'loadNewPosts'
+		},
 		render: function() {
 			var _this = this;
 			this.tiles = new TilesCollection([], {tag: this.options.hash});
 			this.tumblrTiles = new TumblrTilesCollection([], {tag: this.options.hash});
 			this.googleTiles = new GoogleTilesCollection([], {tag: this.options.hash});
+			this.newPostCount = 0;
+			this.alertContainer = this.el.querySelector("#new-post-alert-container");
+			this.alert = this.el.querySelector("#new-post-alert");
 			new LeftView();
 			var loader = _.template(LoaderTemplate);
 			this.$el.html(loader);
 
-			jQuery.when(this.getPosts()).done(jQuery.proxy(this.displayTiles, this));
+			this.getPosts(this.tiles, this.displayTiles);
+			setInterval(function() { _this.poll() }, 30000);
 		},
-		getPosts: function() {
+		getPosts: function(tiles, poll, callback) {
 			var _this = this;
-			return jQuery.when(
+			if(typeof poll == "function") {
+				callback = poll;
+				poll = false;
+			}
+
+			jQuery.when(
 				this.tumblrTiles.fetch({
 					success: function(posts) {
-						_this.tiles.insert(posts.models);
+						var newPosts = _.difference(posts.models, tiles.models);
+						if(poll) _this.newPostCount += newPosts.length;
+						tiles.insert(newPosts);
 					}
 				}),
 				this.googleTiles.fetch({
 					success: function(posts) {
-						_this.tiles.insert(posts.models);
+						var newPosts = _.difference(posts.models, tiles.models);
+						if(poll) _this.newPostCount += newPosts.length;
+						tiles.insert(newPosts);
 					}
 				})
-			).done();
+			).done(jQuery.proxy(callback, this));
 		},
 		displayTiles: function() {
+			var _this = this;
 			var data = {};
 			data.title = this.options.hash;
 			data.tiles = this.tiles.models;
@@ -52,11 +69,29 @@ define([
 			this.$el.html(hashTemplate);
 			var feed = this.el.querySelector('#feed');
 			imagesLoaded(feed, function() {
-				var msnry = new Masonry(feed, {
+				_this.msnry = new Masonry(feed, {
 					itemSelector: ".tile",
-					columnWidth: ".tile"
+					columnWidth: ".tile",
+					stamp: ".stamp"
 				});
 			});
+		},
+		poll: function() {
+			this.getPosts(this.tiles, true, function() {
+				var alertContainer = this.el.querySelector("#new-post-alert-container");
+				var alert = this.el.querySelector("#new-post-alert");
+				if(this.newPostCount > 0) {
+					$(alert).html(this.newPostCount + " new posts! Click to load.");
+					$(alertContainer).show();
+					this.msnry.layout();
+				}
+			});
+		},
+		loadNewPosts: function() {
+			$(this.alertContainer).hide();
+			$(this.alert).html("");
+			this.newPostCount = 0;
+			this.displayTiles();
 		}
 	});
 
